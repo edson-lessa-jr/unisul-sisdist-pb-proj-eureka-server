05dd5b0f594fd4a94c34ccc85b62842c6c25c52d
Edson Lessa <edsonlessajunior@gmail.com>
2021-09-20 21:44:01 -0300

    Primeiro commit

---
+++ src/test/java/br/unisul/aula/projeurekaserver/ProjEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unisul.aula.projeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
